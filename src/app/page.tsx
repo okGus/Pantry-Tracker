fb8ee13a5b23719dd6b9947c4c6b4bac273121fd
Gustavo <rosalesagustavo@gmail.com>
2024-07-29 20:12:50 -0400

    material-ui install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,61 @@
-import Link from "next/link";
+import { Box, Stack } from '@mui/material'
+
+const items = [
+  'tomato',
+  'potato',
+  'onion',
+  'lettuce',
+  'apple',
+  'carrot',
+  'ginger',
+  'kale',
+]
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+    <Box
+      width={'100vw'}
+      height={'100vh'}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      bgcolor={'#ffffff'}
+      flexDirection={'column'}
+    >
+      <Box
+        width={'50rem'}
+        height={'2.5rem'}
+        sx={{m : 1, p: 1}}
+        border={'1px solid #3333'}
+      >
+        <Box>
+          Pantry
+        </Box>
+      </Box>
+      <Stack 
+        width={'800px'} 
+        height={'300px'} 
+        spacing={2} 
+        overflow={'auto'} 
+        border={'1px solid #333'}
+      >
+        {items.map((i) => (
+          <Box
+            key={i}
+            width={'100%'}
+            height={'100px'}
+            display={'flex'}
+            // justifyContent={'center'}
+            // alignItems={'center'}
+            bgcolor={'#f0f0f0'}
+            sx={{ m: 1, p: 1}}
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
-    </main>
+            <Box>
+              {i.charAt(0).toUpperCase() + i.slice(1)}
+            </Box>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
   );
 }
