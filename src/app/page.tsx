b8dd8eca7a577f4ae17b20544fc50d622a663d0f
Gustavo <rosalesagustavo@gmail.com>
2024-08-01 11:00:21 -0400

    openai and header/footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Autocomplete, Box, Button, Collapse, List, ListItemButton, ListItemText, ListSubheader, Modal, Stack, TextField, Typography } from '@mui/material';
+import { Autocomplete, Box, Button, Collapse, Container, Grid, Link, List, ListItemButton, ListItemText, ListSubheader, Modal, Stack, TextField, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { firestore } from '../../firebase';
 import { collection, query, getDocs, deleteDoc, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
@@ -15,8 +15,8 @@ const modalStyle = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
+  border: '1px solid #000',
+  boxShadow: 20,
   p: 4,
 };
 
@@ -33,12 +33,13 @@ export default function HomePage() {
 
   const [collDocs, setCollDocs] = useState<Map<string, string[]>>(new Map());
 
+  const [ingredients, setIngredients] = useState<string>('');
+
   const [openCategory, setOpenCategory] = useState<string | null>(null);
   // const handleCategoryClick = () => setOpenCategory(!openCategory);
   const handleCategoryClick = (category: string) => {
     setOpenCategory(prevCategory => (prevCategory === category ? null : category));
   };
-
 
   const [openModal, setOpenModal] = useState<boolean>(false);
   const handleOpenModal = (item: string) => {
@@ -152,254 +153,245 @@ export default function HomePage() {
   }, [collDocs]);
 
   useEffect(() => {
-    const ingredients: string[] = []
-    pantry.forEach((p) => {
-      const ing = p.name + ' x' + p.count;
-      ingredients.push(ing);
+    const ingredients_: string[] = []
+    pantry.forEach((item) => {
+      const item_ = item.name + ' x' + item.count;
+      ingredients_.push(item_);
     });
-    console.log(ingredients);
+    setIngredients('Ingredients: ' + ingredients_.join(', '));
   }, [pantry]);
 
   return (
     <Box
-      width={'100vw'}
+      width={'100%'}
       height={'100vh'}
       display={'flex'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      bgcolor={'#ffffff'}
+      // justifyContent={'center'}
+      // alignItems={'center'}
+      // bgcolor={'#ffffff'}
       flexDirection={'column'}
     >
-      <Box
-        width={'62.5rem'}
-        height={'2.5rem'}
-        sx={{m : 1, p: 1}}
-        border={'1px solid #3333'}
+      <Box 
+        width="100%" 
+        bgcolor="#f0f4f8" // A light blue-gray color
+        color="#2c3e50" // Dark blue-gray for text
+        py={8}
+        mb={8} 
       >
-        <Typography>
-          Pantry
-        </Typography>
+        <Container maxWidth="lg">
+          <Typography variant="h2" component="h1" gutterBottom>
+            Pantry Tracker
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom>
+            Effortlessly manage your kitchen inventory
+          </Typography>
+          <Typography variant="body1">
+            Keep track of your ingredients, plan meals, and never run out of essentials again.
+          </Typography>
+        </Container>
       </Box>
-      
-        
+
       <Box
+        width={'100%'}
         display={'flex'}
-        // width={'800px'}
-        // height={'300px'}
-        width={'1000px'}
-        height={'500px'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        bgcolor={'#ffffff'}
+        flexDirection={'column'}
       >
         <Box
-          width={'300px'}
-          height={'100%'}
-          border={'1px solid #333'}
-          overflow={'auto'}
+          width={'62.5rem'}
+          height={'2.5rem'}
+          sx={{m : 1, p: 1}}
+          border={'1px solid #3333'}
         >
-          <Autocomplete
-            options={autoCompleteOptions}
-            getOptionLabel={(option) => option}
-            onChange={handleAutoCompleteChange}
-            renderInput={(params) => <TextField {...params} label='Search Item'/>}
-          />
-          {selectedItem ? (
-            <List
-              sx={{width: '100%', maxWidth: 360}}
-              component='nav'
-              subheader={
-                <ListSubheader component='div' id='nested-list-subheader'>
-                </ListSubheader>
-              }
-            >
-              {Array.from(collDocs.entries()).map(([collections, documents]) => (
-                documents.includes(selectedItem) && (
-                  <ListItemButton key={selectedItem}>
-                    <ListItemText primary={selectedItem} onClick={(e) => !isModalActive && handleOpenModal(selectedItem)} />
-                    {/* <Modal
-                      open={openModal && selectedItem == selectedItem}
-                      onClose={handleCloseModal}
-                      slotProps={{ backdrop: {
-                        style: {backgroundColor: 'rgba(0, 0, 0, 0.2)', boxShadow: 'none'},
-                        onClick: (e) => {
-                          e.stopPropagation();
-                          handleCloseModal();
-                        }
-                      }}}
-                    >
-                      <Box sx={modalStyle} component={'div'} onClick={(e) => e.stopPropagation()}>
-                        <Typography variant='h6' component='h2'>
-                          How Many
-                        </Typography>
-                        <Stack width='100%' direction='row' spacing={2}>
-                          <NumberInput 
-                            placeholder='Type a number...'
-                            value={value}
-                            onClick={(e) => e.stopPropagation()}
-                            onChange={(event, val) => setValue(val)}
-                          />
-                          <Button 
-                            variant='outlined'
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              void handleAddItem(collections);
-                              handleCloseModal();
-                            }}
-                          >
-                            Add
-                          </Button>
-                        </Stack>
-                      </Box>
-                    </Modal> */}
-                  </ListItemButton>
-                )
-              ))}
-            </List>
-          ) : (
-          Array.from(collDocs.entries()).map(([collections, documents]) => (
-            <List
-              key={collections}
-              sx={{width: '100%', maxWidth: 360}}
-              component='nav'
-              subheader={
-                <ListSubheader component='div' id='nested-list-subheader'>
-                </ListSubheader>
-              }
-            >
-              <ListItemButton onClick={() => handleCategoryClick(collections)} sx={{backgroundColor: '#E5E5E5'}}>
-                <ListItemText primary={collections} />
-                {openCategory === collections ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-              <Collapse in={openCategory === collections} timeout={'auto'} unmountOnExit>
-                <List component='div' disablePadding>
-                  {documents.map((item) => (
-                    <ListItemButton key={item}>
-                      <ListItemText primary={item} onClick={(e) => !isModalActive && handleOpenModal(item)}/>
-                      {/* <Modal
-                        open={openModal && selectedItem === item}
-                        onClose={handleCloseModal}
-                        slotProps={{ backdrop: {
-                          style: {backgroundColor: 'rgba(0, 0, 0, 0.2)', boxShadow: 'none'},
-                          onClick: (e) => {
-                            e.stopPropagation();
-                            handleCloseModal();
-                          }
-                        }}}
-                      >
-                        <Box sx={modalStyle} component={'div'} onClick={(e) => e.stopPropagation()}>
-                          <Typography variant='h6' component='h2'>
-                            How Many
-                          </Typography>
-                          <Stack width='100%' direction='row' spacing={2}>
-                            <NumberInput 
-                              placeholder='Type a number...'
-                              value={value}
-                              onClick={(e) => e.stopPropagation()}
-                              onChange={(event, val) => setValue(val)}
-                            />
-                            <Button 
-                              variant='outlined'
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                void handleAddItem(item);
-                                handleCloseModal();
-                              }}
-                            >
-                              Add
-                            </Button>
-                          </Stack>
-                        </Box>
-                      </Modal> */}
-                    </ListItemButton>
-                  ))}
-                </List>
-              </Collapse>
-            </List>
-          ))
-        )}
-
-        <Modal
-          open={openModal}
-          onClose={handleCloseModal}
-          slotProps={{ backdrop: {
-            style: {backgroundColor: 'rgba(0, 0, 0, 0.2)', boxShadow: 'none'},
-          }}}
-        >
-          <Box sx={modalStyle} component={'div'} onClick={(e) => e.stopPropagation()}>
-            <Typography variant='h6' component='h2'>
-              How Many {selectedItem}
-            </Typography>
-            <Stack width='100%' direction='row' spacing={2}>
-              <NumberInput 
-                placeholder='Type a number...'
-                value={value}
-                // onClick={(e) => e.stopPropagation()}
-                onChange={(event, val) => setValue(val)}
-              />
-              <Button 
-                variant='outlined'
-                onClick={() => {
-                  // e.stopPropagation();
-                  if (selectedItem) {
-                    void handleAddItem(selectedItem);
-                  }
-                  handleCloseModal();
-                }}
-              >
-                Add
-              </Button>
-            </Stack>
-          </Box>
-        </Modal>
-
+          <Typography>
+            Pantry
+          </Typography>
         </Box>
-
-        <Stack 
-          width={'100%'} 
-          height={'100%'} 
-          spacing={2} 
-          overflow={'auto'} 
-          border={'1px solid #333'}
+        
+          
+        <Box
+          display={'flex'}
+          // width={'800px'}
+          // height={'300px'}
+          width={'1000px'}
+          height={'500px'}
         >
-          {pantry.map((item) => (
-            <Box
-              key={item.name}
-              width={'100%'}
-              minHeight={'50px'}
-              display={'flex'}
-              justifyContent={'space-between'}
-              // alignItems={'center'}
-              bgcolor={'#f0f0f0'}
-              // sx={{ m: 1, p: 1}}
-            >
-              <Box
-                display={'inherit'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                sx={{m: 1, p: 1}}
+          <Box
+            width={'300px'}
+            height={'100%'}
+            border={'1px solid #333'}
+            overflow={'auto'}
+          >
+            <Autocomplete
+              options={autoCompleteOptions}
+              getOptionLabel={(option) => option}
+              onChange={handleAutoCompleteChange}
+              renderInput={(params) => <TextField {...params} label='Search Item'/>}
+            />
+            {selectedItem ? (
+              <List
+                sx={{width: '100%', maxWidth: 360}}
+                component='nav'
+                subheader={
+                  <ListSubheader component='div' id='nested-list-subheader'>
+                  </ListSubheader>
+                }
               >
-                <Typography>
-                  {`${item.name.charAt(0).toUpperCase() + item.name.slice(1)} x ${item.count}`}
-                </Typography>
-              </Box>
-              <Box
-                display={'inherit'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                sx={{m: 1, p: 1}}
+                {Array.from(collDocs.entries()).map(([collections, documents]) => (
+                  documents.includes(selectedItem) && (
+                    <ListItemButton key={selectedItem}>
+                      <ListItemText primary={selectedItem} onClick={(e) => !isModalActive && handleOpenModal(selectedItem)} />
+                    </ListItemButton>
+                  )
+                ))}
+              </List>
+            ) : (
+            Array.from(collDocs.entries()).map(([collections, documents]) => (
+              <List
+                key={collections}
+                sx={{width: '100%', maxWidth: 360}}
+                component='nav'
+                subheader={
+                  <ListSubheader component='div' id='nested-list-subheader'>
+                  </ListSubheader>
+                }
               >
+                <ListItemButton onClick={() => handleCategoryClick(collections)} sx={{backgroundColor: '#E5E5E5'}}>
+                  <ListItemText primary={collections} />
+                  {openCategory === collections ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openCategory === collections} timeout={'auto'} unmountOnExit>
+                  <List component='div' disablePadding>
+                    {documents.map((item) => (
+                      <ListItemButton key={item}>
+                        <ListItemText primary={item} onClick={(e) => !isModalActive && handleOpenModal(item)}/>
+                      </ListItemButton>
+                    ))}
+                  </List>
+                </Collapse>
+              </List>
+            ))
+          )}
+
+          <Modal
+            open={openModal}
+            onClose={handleCloseModal}
+            slotProps={{ backdrop: {
+              style: {backgroundColor: 'rgba(0, 0, 0, 0.2)', boxShadow: 'none'},
+            }}}
+          >
+            <Box sx={modalStyle} component={'div'} onClick={(e) => e.stopPropagation()}>
+              <Typography variant='h6' component='h2'>
+                How Many {selectedItem}
+              </Typography>
+              <Stack width='100%' direction='row' spacing={2}>
+                <NumberInput 
+                  placeholder='Type a number...'
+                  value={value}
+                  // onClick={(e) => e.stopPropagation()}
+                  onChange={(event, val) => setValue(val)}
+                />
                 <Button 
-                  variant="contained"
-                  size='small'
-                  startIcon={<DeleteIcon />}
-                  onClick={() => handleDelete(item.name)}
+                  variant='outlined'
+                  onClick={() => {
+                    // e.stopPropagation();
+                    if (selectedItem) {
+                      void handleAddItem(selectedItem);
+                    }
+                    handleCloseModal();
+                  }}
                 >
-                  <Typography fontSize={12}>Delete</Typography>
+                  Add
                 </Button>
-              </Box>
+              </Stack>
             </Box>
-          ))}
-        </Stack>
+          </Modal>
+
+          </Box>
+
+          <Stack 
+            width={'100%'} 
+            height={'100%'} 
+            spacing={2} 
+            overflow={'auto'} 
+            border={'1px solid #333'}
+          >
+            {pantry.map((item) => (
+              <Box
+                key={item.name}
+                width={'100%'}
+                minHeight={'50px'}
+                display={'flex'}
+                justifyContent={'space-between'}
+                // alignItems={'center'}
+                bgcolor={'#f0f0f0'}
+                // sx={{ m: 1, p: 1}}
+              >
+                <Box
+                  display={'inherit'}
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                  sx={{m: 1, p: 1}}
+                >
+                  <Typography>
+                    {`${item.name.charAt(0).toUpperCase() + item.name.slice(1)} x ${item.count}`}
+                  </Typography>
+                </Box>
+                <Box
+                  display={'inherit'}
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                  sx={{m: 1, p: 1}}
+                >
+                  <Button 
+                    variant="contained"
+                    size='small'
+                    startIcon={<DeleteIcon />}
+                    onClick={() => handleDelete(item.name)}
+                  >
+                    <Typography fontSize={12}>Delete</Typography>
+                  </Button>
+                </Box>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
       </Box>
 
+      <Box
+        width={'100%'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        bgcolor={'#ffffff'}
+        flexDirection={'column'}
+      >
+        <Box>
+          AI
+        </Box>
+      </Box>
+      
+      <Box
+        component="footer"
+        sx={{
+          backgroundColor: '#34495e', // Dark blue-gray
+          color: 'white',
+          py: 6,
+          my: 6,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Grid container spacing={4} justifyContent="space-evenly">  
+          </Grid>
+          <Box mt={5}>
+            <Typography variant="body2" align="center">
+              © {new Date().getFullYear()} Pantry Tracker. All rights reserved.
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
     </Box>
   );
 }
